bd2ba390a9214191f8631b43a438cf3078c94aa7
Yilin Zhang <yzhang0129@outlook.com>
2020-05-14 12:21:28 -0400

    Add eval-paren-region.

--- sclang-helper.el
+++ sclang-helper.el
@@ -32,6 +32,41 @@
       (sclang-helper-eval-region silent-p)
     (sclang-helper-eval-line silent-p)))
 
+;; TODO:
+;; 1. This function should also detect if there's no extra characters around
+;; the outermost parentheses.
+;; 2. It's not functional when the prentheses don't match.
+(defun sclang-helper-eval-paren-region (&optional silent-p)
+  "Execute the region inside the outermost parentheses."
+  (interactive "P")
+  (save-excursion
+    (let ((paren-matched nil))
+      (while
+          (condition-case nil
+              (progn
+                (backward-up-list)
+                (if (equal (char-to-string (char-after)) "(")
+                    (setq paren-matched t)
+                  (setq paren-matched nil))
+                t)
+            (error nil)))
+      (if paren-matched
+          (progn
+            (set-mark (point))
+            (forward-list)
+            (setq mark-active t)
+            (sclang-helper-eval-region-or-line silent-p)
+            (setq mark-active nil)
+            t)
+        nil))))
+
+(defun sclang-helper-auto-eval (&optional silent-p)
+  (interactive "P")
+  (if (and transient-mark-mode mark-active)
+      (sclang-eval-region silent-p)
+    (if (not (sclang-helper-eval-paren-region silent-p))
+        (sclang-helper-eval-line silent-p))))
+
 (defun sclang-helper-show-meter ()
   (interactive)
   (sclang-eval-string "Server.local.meter"))
@@ -48,7 +83,7 @@
 (define-minor-mode sclang-helper-mode
   :lighter " sclang-helper"
   :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "<s-return>") 'sclang-helper-eval-region-or-line)
+            (define-key map (kbd "<s-return>") 'sclang-helper-auto-eval)
             (define-key map (kbd "s-m") 'sclang-helper-show-meter)
             (define-key map (kbd "s-f") 'sclang-helper-show-freqscope)
             (define-key map (kbd "s-n") 'sclang-helper-show-tree)
